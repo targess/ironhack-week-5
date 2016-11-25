b2feef3fd69bbebce56b340b2d334cb12562b90f
Jose Toscano <josetoscanogil@gmail.com>
2016-11-25 12:30:18 +0100

    exercise day5 bonus: artist modal

--- day5/spotify-skeleton/app.js
+++ day5/spotify-skeleton/app.js
@@ -8,6 +8,7 @@ function getTracks(myTracks) {
 	console.log(firstTrack);
 	$('.widget .title').html(firstTrack.name);
 	$('.widget .author').html(firstTrack.artists[0].name);
+	$('.widget .author').data('author-id', firstTrack.artists[0].id);
 	$('.widget .cover img').attr('src', firstTrack.album.images[0].url);
 	$('.js-player').attr('src', firstTrack.preview_url);
 
@@ -16,7 +17,35 @@ function getTracks(myTracks) {
 function printTime() {
 	var current = $('.js-player').prop('currentTime');
 	$('.js-time').val(current);
-	// console.debug('Current time: ' + current);
+};
+
+function printAuthorModal(response) {
+	console.log("on print author modal");
+	
+	var name		 = response.name;
+	var photo		 = response.images[0].url;
+	var genres		 = response.genres.map(function(element){ return element}).toString();
+	var followers	 = response.followers.total;
+	var popularity	 = response.popularity;
+
+	$('#title').text(name);
+	$('#photo').attr('src', photo);
+	$('genres').text(genres);
+	$('followers').text(followers);
+	$('popularity').text(popularity);
+
+	$('.js-modal').modal("show");
+
+};
+
+function getAuthorInfo() {
+	authorId = $('.widget .author').data('author-id');
+
+	$.ajax({
+		url: "https://api.spotify.com/v1/artists/"+authorId,
+		success: printAuthorModal,
+		error: handleErrors
+	});
 };
 
 
@@ -50,4 +79,7 @@ $(document).on ("ready", function(){
 	});
 
 	$('.js-player').on('timeupdate', printTime);
+
+	$('.js-author').on('click', getAuthorInfo);
+
 });
